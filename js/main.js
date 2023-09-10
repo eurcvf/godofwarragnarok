ea514b28eae4e59dbf2d8515514370aedea91126
eurcvf <eurcvf@hotmail.com>
2023-09-10 14:21:32 -0300

    Finalizado aplicação, e iniciando as animações da página.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 var swiper = new Swiper(".slide-character", {
   slidesPerView: 3.5,
   spaceBetween: 19,
-  freeMode: true
+  freeMode: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.1,
+    },
+    768: {
+      slidesPerView: 2.2,
+    },
+    991: {
+      slidesPerView: 2.8,
+    }
+    ,
+    1200: {
+      slidesPerView: 3.5,
+    }
+  }
 });
